b57ea09a716f272c2e1bce338f1a9ab7ea465faa
Nandow22 <125155076+Nandow22@users.noreply.github.com>
2023-02-11 17:28:33 -0300

    Update Nandowshub.lua

--- Nandowshub.lua
+++ Nandowshub.lua
@@ -1,4 +1,4 @@
-local OrionLib =
+local OrionLib = 
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Nandow's Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Nandow's Hub"})
 local Tab = Window:MakeTab({
